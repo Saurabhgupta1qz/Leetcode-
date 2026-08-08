911dc3fbd15ddd926a04850c282cbd94c6bffca0
Saurabhgupta1qz <saurabhgupta1qz@gmail.com>
2026-08-08 15:47:28 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (39.43%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> luckyNumbers(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        vector<int>row;
+        vector<int>col;
+        for(int i = 0; i<n; i++){
+            int mini = INT_MAX;
+            for(int j = 0; j<m; j++){
+                mini = min(mini,matrix[i][j]);
+            }
+            row.push_back(mini);
+        }
+        for(int i = 0; i<m; i++){
+            int maxi = INT_MIN;
+            for(int j = 0; j<n;j++){
+                maxi = max(maxi,matrix[j][i]);
+            }
+            col.push_back(maxi);
+        }
+        vector<int>ans;
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(matrix[i][j]==col[j]&&matrix[i][j]==row[i]){
+                    ans.push_back(matrix[i][j]);
+                }
+            }
+            
+        }
+        return ans;
+    }
+};
