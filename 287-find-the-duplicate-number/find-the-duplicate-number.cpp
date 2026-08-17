383d6d74371eaa505591c2be625cfd8640fdd5c6
Saurabhgupta1qz <saurabhgupta1qz@gmail.com>
2026-08-17 12:21:24 +0530

    Time: 64 ms (19.15%) | Memory: 87.9 MB (12.33%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -13,11 +13,9 @@ public:
         unordered_map<int,int>mp;
         for(int i = 0; i<n; i++){
             mp[nums[i]]++;
+        if(mp[nums[i]]>=2) return nums[i];
+        }
         
-        }
-        for(int i = 0; i<n; i++){
-            if(mp[nums[i]]>=2) return nums[i];
-        }
         return -1;
     }
 };
