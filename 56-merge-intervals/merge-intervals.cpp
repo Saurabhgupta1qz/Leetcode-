b682c16edd82504949e404fcbea9083dbda98bb5
Saurabhgupta1qz <saurabhgupta1qz@gmail.com>
2026-08-17 11:44:42 +0530

    Time: 3 ms (88.92%) | Memory: 23.9 MB (59.56%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+    vector<vector<int>>ans;
+    int n = intervals.size();
+    sort(intervals.begin(),intervals.end());
+    vector<int>cur = intervals[0];
+    for(int i = 1; i<n; i++){
+        if(intervals[i][0]<=cur[1]){
+            cur[1] = max(cur[1],intervals[i][1]);
+        }
+        else {
+            ans.push_back(cur);
+            cur = intervals[i];
+        }
+    }
+    ans.push_back(cur);
+    return ans;
+    }
+};
