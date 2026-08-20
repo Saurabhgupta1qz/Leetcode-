4226af2d472b59b34add25f67d9d3f3e34206dbe
Saurabhgupta1qz <saurabhgupta1qz@gmail.com>
2026-08-20 21:49:56 +0530

    Time: 0 ms (100.00%) | Memory: 28.7 MB (48.86%) - LeetSync

---
+++ 3515-find-if-digit-game-can-be-won/find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        int singlesum = 0;
+        int doublesum = 0;
+        for(int i = 0; i<nums.size(); i++){
+            if(nums[i]/10==0) singlesum+=nums[i];
+            else doublesum+=nums[i];
+        }
+        return singlesum!=doublesum;
+    }
+};
