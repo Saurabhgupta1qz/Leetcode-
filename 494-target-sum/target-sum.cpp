a4a0b210af7e70e5ac90259ba934dc6916f2db46
Saurabhgupta1qz <saurabhgupta1qz@gmail.com>
2026-08-22 18:13:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (68.74%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,3 +1,28 @@
+// class Solution {
+// public:
+//     int findTargetSumWays(vector<int>& nums, int target) {
+//         int n = nums.size();
+//         int sum = 0;
+//         for(int x: nums) sum+=x;
+//         if(abs(target)>sum||(sum+target)%2!=0) return 0;
+//         int mid = (sum+target)/2;
+//         vector<vector<int>>dp(n+1,vector<int>(mid+1,0));
+//         for(int i = 0; i<=n;i++) dp[i][0] = 1;
+//         for(int i = 1; i<=n;i++){
+//             for(int j = 0; j<=mid;j++){
+//                 if(nums[i-1]<=j){
+//                     dp[i][j] = dp[i-1][j]+dp[i-1][j-nums[i-1]];
+//                 }
+//                 else{
+//                     dp[i][j] = dp[i-1][j];
+//                 }
+//             }
+//         }
+//         return dp[n][mid];
+//     }
+// };
+
+
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -6,18 +31,13 @@ public:
         for(int x: nums) sum+=x;
         if(abs(target)>sum||(sum+target)%2!=0) return 0;
         int mid = (sum+target)/2;
-        vector<vector<int>>dp(n+1,vector<int>(mid+1,0));
-        for(int i = 0; i<=n;i++) dp[i][0] = 1;
+        vector<int>dp(mid+1,0);
+        dp[0] = 1;
         for(int i = 1; i<=n;i++){
-            for(int j = 0; j<=mid;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j] = dp[i-1][j]+dp[i-1][j-nums[i-1]];
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
+            for(int j = mid; j>=nums[i-1];j--){
+                dp[j] = dp[j]+dp[j-nums[i-1]];
             }
         }
-        return dp[n][mid];
+        return dp[mid];
     }
 };
